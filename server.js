6bbb0af43a83bd34e53e5f33a65669ea93eca14e
bhuvaneshwaribhuna83-dot <bhuvaneshwaribhuna83@gmail.com>
2026-07-13 19:55:37 +0530

    Add health check endpoint and refactor recipe generation

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("AI Recipe Generator Backend Running");
+});
 
 app.post("/generate-recipe", async (req, res) => {
 
